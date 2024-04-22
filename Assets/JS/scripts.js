68f80d67214c8b72b53184445a76642ae502ae41
Sevenwings26 <iarowosola@yahoo.com>
2024-04-22 16:35:49 +0100

    update project

--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -1,36 +1,45 @@
-// toggle for small screens 
-const toggleDisplay = document.getElementById('togg-display');
+// JavaScript for modal toggling
+const loginBtn = document.getElementById("loginBtn");
+const SignBtn = document.getElementById("SignBtn");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close");
+const loginBtnMobile = document.getElementById("loginBtnMobile");
+const SignBtnMobile = document.getElementById("SignBtnMobile");
 
-let navLink = document.querySelector(".nav-links2");
-function toggle(){
-
-
-    if (toggleDisplay.style.display === 'none'){
-        toggleDisplay.style.display = 'block'
-        navLink.style.width = "100%"
-        navLink.style.display = "flex"
-    } else {
-        toggleDisplay.style.display = 'none';
-    }
-}
-
-
-function cancel() {
-    toggleDisplay.style.display = 'none';    
-}
-
-
-const viewLoginModal = document.querySelector('#login-view');
-
-let modalView = document.getElementById('login-modal');
-
-viewLoginModal.addEventListener('click', function() {
-    // alert('')
-    if (modalView.style.display === none){
-    
-    modalView.style.display = 'block';
-}else {
-    modalView.style.display = 'none';
-}
-
+loginBtn.addEventListener("click", () => {
+    modal.style.display = "block";
 });
+
+SignBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+loginBtnMobile.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+SignBtnMobile.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+// Function to toggle the hamburger menu
+function toggle() {
+    var menuToggle = document.getElementById("togg-display");
+    menuToggle.style.display = "flex";
+}
+
+// Function to cancel the hamburger menu
+function cancel() {
+    var menuToggle = document.getElementById("togg-display");
+    menuToggle.style.display = "none";
+}
